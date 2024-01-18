4a51ebc7784b3b36899ee8ee97b8e64848e57d8e
hamza1100 <ahmedhamza1995@hotmail.com>
2024-01-18 11:56:51 +0000

    deploy django app using openAI APIs on AWS

--- backend/django_chatgpt/settings.py
+++ backend/django_chatgpt/settings.py
@@ -9,8 +9,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = os.environ.get("SECRET_KEY")
 DEBUG = int(os.environ.get("DEBUG", default=0))
-
-ALLOWED_HOSTS = os.environ.get("DJANGO_ALLOWED_HOSTS").split(" ")
+# ALLOWED_HOSTS = os.environ.get("DJANGO_ALLOWED_HOSTS").split(" ")
+ALLOWED_HOSTS = ['*']
 DATABASES = {
     "default": {
         "ENGINE": os.environ.get("DB_ENGINE", "django.db.backends.sqlite3"),
